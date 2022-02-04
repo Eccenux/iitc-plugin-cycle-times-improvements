39312b28560b430e24919acdf3c27b8865f66f42
Nux <eccenux+git@gmail.com>
2022-02-04 22:52:54 +0100

    0.2.2: intel url update

--- cycle-times-improvements.meta.js
+++ cycle-times-improvements.meta.js
@@ -2,9 +2,9 @@
 // @id             iitc-plugin-cycle-times-improvements@jonatkins
 // @name           IITC plugin: Show cycle/checkpoint times improved
 // @category       Info
-// @version        0.2.1
+// @version        0.2.2
 // @namespace      https://github.com/jonatkins/ingress-intel-total-conversion
-// @description    [0.2.1] Show the times used for the septicycle and checkpoints. Additionaly shows delta time info.
+// @description    [0.2.2] Show the times used for the septicycle and checkpoints. Additionaly shows delta time info.
 // @include        https://*.ingress.com/intel*
 // @include        http://*.ingress.com/intel*
 // @match          https://*.ingress.com/intel*
@@ -13,5 +13,6 @@
 // @include        http://*.ingress.com/mission/*
 // @match          https://*.ingress.com/mission/*
 // @match          http://*.ingress.com/mission/*
+// @match          https://intel.ingress.com/*
 // @grant          none
 // ==/UserScript==
--- cycle-times-improvements.user.js
+++ cycle-times-improvements.user.js
@@ -2,9 +2,9 @@
 // @id             iitc-plugin-cycle-times-improvements@jonatkins
 // @name           IITC plugin: Show cycle/checkpoint times improved
 // @category       Info
-// @version        0.2.1
+// @version        0.2.2
 // @namespace      https://github.com/jonatkins/ingress-intel-total-conversion
-// @description    [0.2.1] Show the times used for the septicycle and checkpoints. Additionaly shows delta time info.
+// @description    [0.2.2] Show the times used for the septicycle and checkpoints. Additionaly shows delta time info.
 // @include        https://*.ingress.com/intel*
 // @include        http://*.ingress.com/intel*
 // @match          https://*.ingress.com/intel*
@@ -13,6 +13,7 @@
 // @include        http://*.ingress.com/mission/*
 // @match          https://*.ingress.com/mission/*
 // @match          http://*.ingress.com/mission/*
+// @match          https://intel.ingress.com/*
 // @grant          none
 // @updateURL      https://github.com/Eccenux/iitc-plugin-cycle-times-improvements/raw/master/cycle-times-improvements.meta.js
 // @downloadURL    https://github.com/Eccenux/iitc-plugin-cycle-times-improvements/raw/master/cycle-times-improvements.user.js
